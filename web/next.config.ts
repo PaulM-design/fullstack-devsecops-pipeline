77d35fb9bb07011b1b9a28c1a5235eb1dd358832
PaulM-design <paulmus7@proton.me>
2026-08-12 12:30:21 +1000

    Conditionally set output based on VERCEL environment

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  ...(process.env.VERCEL ? {} : { output: "standalone" }),
   poweredByHeader: false,
 };
 
 export default nextConfig;
-
